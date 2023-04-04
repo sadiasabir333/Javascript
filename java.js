230562d0c885e6a257bb1ed63010630266ce2246
sadiasabir <sadiasabir2002@gimal.com>
2023-04-04 23:20:12 +0500

    javascript GIT

---
+++ java.js
@@ -0,0 +1,28 @@
+//Alert in in-browser Javascript
+alert("This is a message");
+alert("Error!pleas enter a valid passed");
+alert("Welcome to js lang\nHappy coding");
+
+alert("Welcome to js land");
+ 
+let name = prompt("What is your name?","Guest");
+console.log(name);
+let Happy  =prompt ("Happy coding?","gprevent this page from creating additional dialogs");
+
+alert("Hello!I can run js through my web browser's console");
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+
+var rightNow = new Date();
+var rightNowTOstring = rightNow.toString();
+document.write(rightNowTOstring);
+document.write("<br/>");
+vartoday = rightNowTOstring.slice(0,8);
+varmonth = rightNowTOstring.slice(3,7);
+varyear = rightNowTOstring.slice(10,15);
+document.write("Today is" +"" +   today);
+document.write("<br/>");
+document.write("Month is" +"" + moth);
+ document.write("<br/>");
